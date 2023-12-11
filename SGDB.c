f0b919980c5d4c1dc4d3e6ae1c70721f71ab9f2e
Vinilimm <viniciuscarneiro.1001@hotmail.com>
2023-12-11 19:57:08 -0300

    texto do menu feito

--- SGDB.c
+++ SGDB.c
@@ -3,5 +3,16 @@
 
 void printmenu()
 {
-    printf("Hallo uordi");
+    printf(
+    "Olá, bem vindo ao SGBD da disciplina de ITP.
+    Digite o número da opção desejada:
+    1.Criar um tabela
+    2.Listar todas as tabelas
+    3.Criar uma nova linha na tabela
+    4.Listar todos os dados de uma tabela
+    5.Pesquisar valor em uma tabela
+    6.Apagar uma tupla (registro ou linha) de uma tabela
+    7.Apagar uma tabela
+    8.Sair do menu");
 }
+
